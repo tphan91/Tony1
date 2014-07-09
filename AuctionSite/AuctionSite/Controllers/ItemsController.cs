c997a67faa6ba26e9431ed0d37ee5f7d47c8c05e
tphan91 <tonyphan.y2k5@gmail.com>
2014-07-09 14:33:49 +0100

    YOYOYO

---
+++ AuctionSite/AuctionSite/Controllers/ItemsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AuctionSite.Models;
+using AuctionSite.DAL;
+
+namespace AuctionSite.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private AuctionContext db = new AuctionContext();
+
+        //
+        // GET: /Items/
+
+        public ViewResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        //
+        // GET: /Items/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Items items = db.Items.Find(id);
+            return View(items);
+        }
+
+        //
+        // GET: /Items/Create
+
+        public ActionResult Create()
+        {
+            return RedirectToAction("Create", "Bids");
+           // return View();
+        }
+
+        //
+        // POST: /Items/Create
+
+        //[HttpPost]
+        //public ActionResult Create(Items items)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Items.Add(items);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");  
+        //    }
+
+        //    return View(items);
+        //}
+
+        //
+        // GET: /Items/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Items items = db.Items.Find(id);
+            return View(items);
+        }
+
+        //
+        // POST: /Items/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Items items)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(items).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(items);
+        }
+
+        //
+        // GET: /Items/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Items items = db.Items.Find(id);
+            return View(items);
+        }
+
+        //
+        // POST: /Items/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Items items = db.Items.Find(id);
+            db.Items.Remove(items);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AuctionSite/AuctionSite/Models/Items.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AuctionSite.Models;
+using System.ComponentModel;
+
+namespace AuctionSite.Models
+{
+    public class Items : BaseClass
+    {
+
+        public int ItemsID { get; set; }
+
+        [DisplayName("Valuation")]
+        public decimal valuation { get; set; }
+
+        [DisplayName("Artist")]
+        public string artist { get; set; }
+
+        [DisplayName("Title")]
+        public string title { get; set; }
+
+        [DisplayName("Condition")]
+        public string condition { get; set; }
+
+        [DisplayName("Description")]
+        public string description { get; set; }
+
+        public virtual ICollection<Bids> Bids { get; set; }
+    }
+}
